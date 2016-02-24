9600ae3bedb8db4e2cb5de160f8b060c3e9a4b76
Evgenii Sopov <evgenii@videoexpertsgroup.com>
2016-02-24 16:40:01 +0600

    updated scripts

--- record.sh
+++ record.sh
@@ -4,6 +4,6 @@ if [ -f "output.avi" ]; then
 	rm output.avi
 fi
 
-./visualization --framerate 5 | \
+./visualization | \
 	ffmpeg -f rawvideo -pixel_format rgb32 -video_size 1280x720 \
-	-framerate 5 -i pipe:0 -an -vcodec mpeg4 output.avi
+	-framerate 10 -i pipe:0 -an -vcodec mpeg4 output.avi
--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 ## avconv -> ffmpeg
 
-./visualization --framerate 10 | \
-	avplay -f rawvideo -pixel_format rgb32 -video_size 1280x720 \
+./visualization | \
+	ffplay -f rawvideo -pixel_format rgb32 -video_size 1280x720 \
 	-framerate 10 -i pipe:0
